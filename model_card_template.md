b28cbd3e82bf7d5b0f4cf7dc88aa2f4767e9b7bd
Josh Gray <jgray10315@gmail.com>
2026-08-16 18:02:23 +0000

    Wrote model card, ran unit tests, constructed RESTful API with FastAPI.

--- model_card_template.md
+++ model_card_template.md
@@ -3,16 +3,16 @@
 For additional information see the Model Card paper: https://arxiv.org/pdf/1810.03993.pdf
 
 ## Model Details
-
+The model was developed by Josh Gray and is a RandomForestClassifier imported from sklearn.ensemble.  The model was trained using default hyperparameters.
 ## Intended Use
-
+The model is used to classify whether an individual's salary is more than $50K or less than or equal to $50K based on census features.  Intended users are data scientists interested in this census classification.
 ## Training Data
-
+According to UC Irvine Machine Learning Repository, Barry Becker extracted the data from the 1994 Census database.The census data was provided as a CSV file in the data folder in the Deploying-a-Scalable-ML-Pipeline-with-FastAPI repository.  The training set consisted of 80% of the data.  OneHotEncoder was used on training data features, and LabelBinarizer was used on the target label.
 ## Evaluation Data
-
+The test set consisted of 20% of the data.  The OneHotEncoder and LabelBinarizer fit on the training set were reused to process the test set.
 ## Metrics
-_Please include the metrics used and your model's performance on those metrics._
-
+The metrics that were used were precision, recall, and F1 score.  After running train_model.py, these metrics were produced - Precision: 0.7440 | Recall: 0.6346 | F1: 0.6850.  The slice_output.txt file contains the same 3 metrics for different slices of data, showing that model performace varies across categories.
 ## Ethical Considerations
-
+Because the census data contains demographic information, the model is at risk for unwanted bias.  The model could over-represent some groups and under-represent others.
 ## Caveats and Recommendations
+Something to consider is that the model predicts based on census data from 1994.  Any inferences that can be made should not be applied to the current socioeconomic environment.  The model's use should be limited to educational purposes only.
--- test_ml.py
+++ test_ml.py
@@ -1,28 +1,66 @@
 import pytest
-# TODO: add necessary import
+from ml.model import compute_model_metrics, train_model, inference
 
-# TODO: implement the first test. Change the function name and input as needed
-def test_one():
+def test_compute_model_metrics():
     """
-    # add description for the first test
+    A test that computes model metrics on simple true labels and predicted labels.  This function tests
+    the compute_model_metrics() function against known values for precision, recall, and fbeta.
     """
-    # Your code here
-    pass
+    y = [1, 1, 0, 0]
+    preds = [1, 0, 1, 0]
 
+    expected_precision = 0.5
+    expected_recall = 0.5
+    expected_fbeta = 0.5
+    expected_metrics = expected_precision, expected_recall, expected_fbeta
 
-# TODO: implement the second test. Change the function name and input as needed
-def test_two():
-    """
-    # add description for the second test
-    """
-    # Your code here
-    pass
+    returned_precision, returned_recall, returned_fbeta = compute_model_metrics(y, preds)
+    returned_metrics = returned_precision, returned_recall, returned_fbeta
 
+    assert returned_metrics == expected_metrics
 
-# TODO: implement the third test. Change the function name and input as needed
-def test_three():
+def test_train_model():
     """
-    # add description for the third test
+    A test that trains the model with simple training sets.  The function tests that train_model()
+    returns a model object.
     """
-    # Your code here
-    pass
+    X_train = [
+        [32, 40],
+        [46, 40],
+        [19, 20],
+        [24, 36]
+    ]
+
+    y_train = [1, 1, 0, 0]
+
+    model_object = train_model(X_train, y_train)
+
+    assert model_object is not None
+
+def test_inference():
+    """
+    A test that uses the train_model() function to create a model to be used as input for 
+    inference().  This function tests that inference() returns the correct number of predictions
+    by using the provided model and a simple testing set.
+    """
+    X_train = [
+        [32, 40],
+        [46, 40],
+        [19, 20],
+        [24, 36]
+    ]
+
+    y_train = [1, 1, 0, 0]
+
+    model = train_model(X_train, y_train)
+
+    X = [
+        [68, 40],
+        [29, 40],
+        [18, 16],
+        [48, 34]
+    ]
+
+    preds = inference(model, X)
+
+    assert len(preds) == len(X)
